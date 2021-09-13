e8a95b0507b3702ebf217298810fa540e92b4194
Redflame39 <vanya133719@gmail.com>
2021-09-13 08:16:11 +0300

    TimeTracer implementation, TraceResult entity

---
+++ Out/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Out
+{
+
+    public interface ITracer
+    {
+        void StartTrace();
+
+        void StopTrace();
+
+ 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    struct TraceResult
+    {
+
+    }
+}
