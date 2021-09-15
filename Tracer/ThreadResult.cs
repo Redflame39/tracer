3f8352c4d55d0cfb46dc1f83fbaec1cb39aed7ba
Redflame39 <vanya133719@gmail.com>
2021-09-15 15:38:12 +0300

    ThreadResult implemented

---
+++ Tracer/ThreadResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    class ThreadResult
+    {
+        private int _threadId;
+        private int _executionTime;
+        private List<MethodResult> _methods;
+
+        public ThreadResult()
+        {
+            _threadId = Thread.CurrentThread.ManagedThreadId;
+            _methods = new List<MethodResult>();
+        }
+
+        public int ThreadId { get; }
+
+        public int ExecutionTime { get; }
+
+        public List<MethodResult> Methods { 
+            get
+            {
+                return new List<MethodResult>(_methods);
+            }
+        }
+
+        public void AddMethod(MethodResult method)
+        {
+            _methods.Add(method);
+        }
+        
+    }
+}
---
+++ Tracer/TimeTracer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Tracer
+{
+    public class TimeTracer : ITracer
+    {
+
+        private TraceResult _result;
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private string _callerMethodName;
+        private string _callerClassName;
+
+        public TraceResult GetTraceResult()
+        {
+            return _result;
+        }
+
+        public void StartTrace()
+        {
+            _startTime = DateTime.Now;
+            StackTrace s = new StackTrace(0);
+            StackFrame callerFrame = s.GetFrame(1);
+            _callerMethodName = callerFrame.GetMethod().Name;
+            _callerClassName = callerFrame.GetMethod().DeclaringType.Name;
+        }
+
+        public void StopTrace()
+        {
+            _stopTime = DateTime.Now;
+
+            long startTicks = _startTime.Ticks;
+            long stopTicks = _stopTime.Ticks;
+            long executionTimeTicks = startTicks - stopTicks;
+            _result = new TraceResult();
+            _result.ExecutionTime = executionTimeTicks;
+            _result.MethodName = _callerMethodName;
+            _result.ClassName = _callerClassName;
+        }
+    }
+}
