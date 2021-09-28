5137fb9e662b4b335b740c7fe9313d8d4b5f0b01
Redflame39 <vanya133719@gmail.com>
2021-09-28 16:06:16 +0300

    serializers

---
+++ Out/Serialization/XmlResultSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Tracer;
+
+namespace Out
+{
+    public class XmlResultSerializer : ISerializer
+    {
+        public string Serialize(TraceResult result)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(TraceResult));
+            using (StringWriter textWriter = new StringWriter())
+            {
+                serializer.Serialize(textWriter, result);
+                return textWriter.ToString();
+            }
+        }
+    }
+}
